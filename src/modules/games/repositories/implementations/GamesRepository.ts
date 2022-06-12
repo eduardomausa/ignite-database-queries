0cbd2601a6b229943b5c616d2c04a1099e32695c
eduardomausa <mausaeduardo@gmail.com>
2022-06-12 09:28:12 -0300

    Database challenge

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -14,12 +14,13 @@ export class GamesRepository implements IGamesRepository {
 
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
-  }
+      .createQueryBuilder("game")
+      .where("game.title ILIKE :title", { title: `%${param}%` })
+      .getMany();
+    }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('SELECT COUNT(*) FROM games');
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
